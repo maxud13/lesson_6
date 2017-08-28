6173009b01dea1f943f79069f4008e520cddcbb6
maxud <mlenon13@gmail.com>
2017-08-28 19:50:46 +0300

    добавлен экран для домашнего задания

---
+++ lesson_6/lesson_6/CoffeeViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoffeeViewController.h
+//  lesson_6
+//
+//  Created by maxud on 28.08.17.
+//  Copyright © 2017 lesson_1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoffeeViewController : UIViewController
+
+@end
---
+++ lesson_6/lesson_6/CoffeeViewController.m
@@ -0,0 +1,26 @@
+//
+//  CoffeeViewController.m
+//  lesson_6
+//
+//  Created by maxud on 28.08.17.
+//  Copyright © 2017 lesson_1. All rights reserved.
+//
+
+#import "CoffeeViewController.h"
+
+@interface CoffeeViewController ()
+
+@end
+
+@implementation CoffeeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+- (IBAction)backAction:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+@end
